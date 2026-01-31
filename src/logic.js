4c3d5e194f847a36f0d3122069b9e464d415d61e
Satyam vivek <svivek@techrayslabs.com>
2026-01-31 10:29:37 +0530

    added dot container to switch between diffrent backgrounds

--- src/logic.js
+++ src/logic.js
@@ -1,134 +1,3 @@
-
-
-// const prevBtn = document.getElementById("prevBtn");
-// const nextBtn = document.getElementById("nextBtn");
-
-// const navigationBtns = document.getElementById("navigationBtns");
-// const slowBtn = document.getElementById("slowBtn");
-// const fastBtn = document.getElementById("fastBtn");
-// const progressBar = document.getElementById("progressBar");
-// const timerBtn = document.getElementById("timer");
-
-// aboutBtn.addEventListener("click", () => {
-  //   aboutOverlay.style.display = "flex";
-  //   bg.style.filter = `brightness(0.7) blur(12px)`;
-  //   bar.style.filter = `brightness(0.7) blur(12px)`;
-  //   bar.style.pointerEvents = "none";
-  //   prevBtn.style.pointerEvents = "none";
-  //   prevBtn.style.filter = "blur(12px)";
-  //   nextBtn.style.pointerEvents = "none";
-  //   nextBtn.style.filter = "blur(12px)";
-  //   pauseBtn.style.pointerEvents = "none";
-  //   pauseBtn.style.filter = "blur(12px)";
-  //   slowBtn.style.pointerEvents = "none";
-  //   slowBtn.style.filter = "blur(12px)";
-  //   fastBtn.style.pointerEvents = "none";
-  //   fastBtn.style.filter = "blur(12px)";
-  //   progressBar.style.filter = "blur(12px)";
-  //   timerBtn.style.filter = "blur(12px)";
-  
-  // });
-  
-  // testimonialsBtn.addEventListener("click", () => {
-    //   testimonialsOverlay.style.display = "flex";
-    //   bg.style.filter = `brightness(0.7) blur(12px)`;
-    //   bar.style.filter = `brightness(0.7) blur(12px)`;
-    //   bar.style.pointerEvents = "none";
-    //   prevBtn.style.pointerEvents = "none";
-    //   prevBtn.style.filter = "blur(12px)";
-    //   nextBtn.style.pointerEvents = "none";
-    //   nextBtn.style.filter = "blur(12px)";
-    //   pauseBtn.style.pointerEvents = "none";
-    //   pauseBtn.style.filter = "blur(12px)";
-    //   slowBtn.style.pointerEvents = "none";
-    //   slowBtn.style.filter = "blur(12px)";
-    //   fastBtn.style.pointerEvents = "none";
-    //   fastBtn.style.filter = "blur(12px)";
-    //   progressBar.style.filter = "blur(12px)";
-    //   timerBtn.style.filter = "blur(12px)";
-    
-    // });
-    
-    // contactBtn.addEventListener("click", () => {
-      //   contactOverlay.style.display = "flex";
-      //   bg.style.filter = `brightness(0.7) blur(12px)`;
-      //   bar.style.filter = `brightness(0.7) blur(12px)`;
-      //   bar.style.pointerEvents = "none";
-      //   prevBtn.style.pointerEvents = "none";
-      //   prevBtn.style.filter = "blur(12px)";
-      //   nextBtn.style.pointerEvents = "none";
-      //   nextBtn.style.filter = "blur(12px)";
-      //   pauseBtn.style.pointerEvents = "none";
-      //   pauseBtn.style.filter = "blur(12px)";
-      //   slowBtn.style.pointerEvents = "none";
-      //   slowBtn.style.filter = "blur(12px)";
-      //   fastBtn.style.pointerEvents = "none";
-      //   fastBtn.style.filter = "blur(12px)";
-      //   progressBar.style.filter = "blur(12px)";
-      //   timerBtn.style.filter = "blur(12px)";
-      
-      // });
-      
-      // closeBtn.forEach((btn) => {
-        //   btn.addEventListener("click", () => {
-          
-          //     aboutOverlay.style.display = "none";
-          //     testimonialsOverlay.style.display = "none";
-          //     contactOverlay.style.display = "none";
-          
-          //     bg.style.filter = `brightness(0.7) blur(0px)`;
-          
-          
-//     bar.style.filter = `blur(0px)`;
-//     bar.style.pointerEvents = "auto";
-
-//     prevBtn.style.pointerEvents = "auto";
-//     prevBtn.style.filter = "blur(0px)";
-
-//     nextBtn.style.pointerEvents = "auto";
-//     nextBtn.style.filter = "blur(0px)";
-//     pauseBtn.style.pointerEvents = "auto";
-//     pauseBtn.style.filter = "blur(0px)";
-
-//     navigationBtns.style.filter = "blur(0px)";
-
-//     progressBar.style.filter = "blur(0px)";
-
-//     slowBtn.style.pointerEvents = "auto";
-//     slowBtn.style.filter = "blur(0px)";
-//     fastBtn.style.pointerEvents = "auto";
-//     fastBtn.style.filter = "blur(0px)";
-
-//     timerBtn.style.filter = "blur(0px)";
-
-//   });
-// });
-
-
-
-// })
-
-// eyeBtn.addEventListener("click", () => {
-
-//   if(bar.style.display == "flex"){
-//     bar.style.display = "none";
-//     prevBtn.style.display = "none";
-//     nextBtn.style.display = "none";
-//     navigationBtns.style.display = "none";
-//     pauseBtn.style.display = "none";
-//     progressBar.style.display = "none";
-//   }
-
-//   else{
-//     bar.style.display = "flex";
-//     prevBtn.style.display = "flex";
-//     nextBtn.style.display = "flex";
-//     navigationBtns.style.display = "flex";
-//     pauseBtn.style.display = "flex";
-//     progressBar.style.display = "block";
-//   }
-
-// })
 const aboutBtn = document.getElementById("about");
 const testimonialsBtn = document.getElementById("testimonials");
 const contactBtn = document.getElementById("contact");
@@ -174,6 +43,7 @@ const imgDes8 = document.getElementById("imgDes8");
 let change = document.getElementsByClassName("change");
 const pauseSvg = document.getElementById("pauseSvg");
 const playSvg = document.getElementById("playSvg");
+const dotContainer =document.getElementById("dotsContainer");
 
 
 let currentIndex = 0;
@@ -251,7 +121,7 @@ function pausebg() {
       playSvg.classList.remove('hidden');
       flag = 1;
   }
-  
+
   else if(flag == 1){
       startTimer();
       pauseSvg.classList.remove('hidden')
@@ -353,8 +223,8 @@ testimonialsBtn.addEventListener("click", () => {
   testimonialsOverlay.classList.remove("hidden");
   testimonialsOverlay.classList.add("flex");
   blurbackground();
-  
-  
+
+
 
 })
 
@@ -368,8 +238,8 @@ contactBtn.addEventListener("click", () => {
 
 closeBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
-    
-    
+
+
     aboutOverlay.classList.remove("flex");
     aboutOverlay.classList.add("hidden");
 
@@ -418,7 +288,14 @@ eyeBtn.addEventListener("click", () => {
       fastBtn.classList.remove("flex");
       fastBtn.classList.add("hidden");
 
+      dotContainer.classList.remove("flex");
+      dotContainer.classList.add("hidden");
+
+      imgDes.classList.remove("flex");
+      imgDes.classList.add("hidden");
+
       progressBar.classList.add("hidden");
+
 
     }
 
@@ -445,7 +322,13 @@ eyeBtn.addEventListener("click", () => {
       fastBtn.classList.remove("hidden");
       fastBtn.classList.add("flex");
 
+      dotContainer.classList.remove("hidden");
+      dotContainer.classList.add("flex");
+
       progressBar.classList.remove("hidden");
+
+      imgDes.classList.remove("hidden");
+      imgDes.classList.add("flex");
 
     }
 })
@@ -470,6 +353,9 @@ function blurbackground() {
 
   fastBtn.classList.add("blur-md");
   fastBtn.style.pointerEvents = "none";
+
+  dotContainer.classList.add("blur-md");
+  dotContainer.style.pointerEvents = "none";
 
   timerShow.classList.add("blur-md");
 
@@ -498,6 +384,9 @@ function unblurBackground() {
   slowBtn.classList.remove("blur-md");
   slowBtn.style.pointerEvents = "auto";
 
+  dotContainer.classList.remove("blur-md");
+  dotContainer.style.pointerEvents = "auto";
+
   fastBtn.classList.remove("blur-md");
 
   fastBtn.style.pointerEvents = "auto";
@@ -509,11 +398,3 @@ function unblurBackground() {
   imgDes.classList.remove("blur-md");
 
 }
-
-
-
-
-
-
-
-
